db7235d767180174b22b1edffe93b41248c620fc
Santiago Perera <santiagoperera07@gmail.com>
2025-06-02 19:48:06 -0300

    Se creó el componente Login y el componente Registro y ambos quedaron funcionando correctamente.

--- gestiona662/App.jsx
+++ gestiona662/App.jsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Test from './components/Test';
+import Login from './components/Login';
+import Registro from './components/Registro';
 
 export default function App() {
   let nombre = 'Gestión de Activos';
   return (
     <>
       <View style={styles.container}>
-        <Text>Título</Text>
-        <Text>{nombre}</Text>
-        <Test />
+        {/* <Login/> */}
+        <Registro/>
         <StatusBar style="auto" />
       </View>
 
