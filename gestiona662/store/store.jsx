ba2d016f4ae3ee75f126da243e408078ffb1c1fc
jricca <jricca19@gmail.com>
2025-06-08 11:57:04 -0300

    complemento para dev tools

--- gestiona662/store/store.jsx
+++ gestiona662/store/store.jsx
@@ -1,8 +1,9 @@
 import { legacy_createStore as createStore, combineReducers } from "@reduxjs/toolkit";
 import usuarioReducer from "./slices/usuarioSlice"
+import composeWithDevTools from "redux-devtools-expo-dev-plugin";
 
 const rootReducer = combineReducers({
     usuario:usuarioReducer
 });
 
-export const store=createStore(rootReducer);
+export const store=createStore(rootReducer, composeWithDevTools());
